a126448ecbcb51bb50b137cb5334e5295b578c91
Polina Koleva <polina@Polinas-MacBook-Pro.local>
2016-08-02 18:41:53 +0200

    Add the main class.

---
+++ BasicDataStructures/src/com/data/structutes/simple/queue/MainExecutionPoint.java
@@ -0,0 +1,21 @@
+/**
+ * This code is produced with learning purposes.
+ * 
+ * Polina Koleva, 02.08.2016.
+ */
+package com.data.structutes.simple.queue;
+
+/**
+ * Main class which is used for testing the implemented data structures.
+ * 
+ * @author Polina Koleva
+ *
+ */
+public class MainExecutionPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
