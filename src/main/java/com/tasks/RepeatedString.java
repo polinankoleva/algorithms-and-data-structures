4c639838851362b7daf7685ce977b47ba78cbb4b
polina <polina.n.koleva@gmail.com>
2018-11-11 18:47:56 +0100

    Addition to the previous commit

---
+++ src/main/java/com/tasks/RepeatedString.java
@@ -0,0 +1,37 @@
+package com.tasks;
+
+import java.util.Scanner;
+
+public class RepeatedString {
+
+	// Complete the repeatedString function below.
+	static long repeatedString(String s, long n) {
+		// count number of a in the string s
+		String s1 = s;
+		long count = s.length() - s1.replace("a", "").length();
+		long repeat = n / s.length();
+		int remainder = (int) (n % s.length());
+		long count2 = 0;
+		if (remainder != 0) {
+			String repeatedSubstring = s.substring(0, remainder);
+			count2 = repeatedSubstring.length() - repeatedSubstring.replace("a", "").length();
+		}
+		return repeat * count + count2;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+	
+		String s = scanner.nextLine();
+
+		long n = scanner.nextLong();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		long result = repeatedString(s, n);
+
+		System.out.println(result);
+
+		scanner.close();
+	}
+}
---
+++ src/main/java/com/tasks/TwoStrings.java
@@ -0,0 +1,43 @@
+package com.tasks;
+
+import java.util.Scanner;
+
+public class TwoStrings {
+
+	static String twoStrings(String s1, String s2) {
+		String smaller = null;
+		String bigger = null;
+		if (s1.length() < s2.length()) {
+			smaller = s1;
+			bigger = s2;
+		} else {
+			smaller = s2;
+			bigger = s1;
+		}
+		for (int i = 0; i < smaller.length(); i++) {
+			String currentLetter = smaller.substring(i, i+1);
+			if (bigger.contains(currentLetter)) {
+				return "YES";
+			}
+		}
+		return "NO";
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int q = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int qItr = 0; qItr < q; qItr++) {
+			String s1 = scanner.nextLine();
+
+			String s2 = scanner.nextLine();
+
+			String result = twoStrings(s1, s2);
+			System.out.println(result);
+		}
+
+		scanner.close();
+	}
+}
