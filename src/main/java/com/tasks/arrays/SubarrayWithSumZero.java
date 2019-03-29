9c1f171ee59a4609fb11dfbc3a86adbab321c48f
polinankoleva <polina.n.koleva@gmail.com>
2019-03-29 13:47:35 +0200

    More tasks solved with hashing

--- src/main/java/com/tasks/arrays/SubarrayWithSumZero.java
+++ src/main/java/com/tasks/arrays/SubarrayWithSumZero.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 /**
  * Given an array of integers, find length of the largest subarray with sum
- * equals to 0.
+ * equals to 0. Addition - find the number of subarrays with sum equals to 0.
  * 
  * @author Polina
  *
@@ -36,12 +36,37 @@ public class SubarrayWithSumZero {
 		return maxLenght;
 	}
 
+	// Complexity O(n), negative and positive numbers
+	public static int subArraySumZeroCount(int[] arr) {
+		// number, sum count
+		HashMap<Integer, Integer> sums = new HashMap<>();
+		int count = 0;
+		int currSum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			currSum += arr[i];
+			if (currSum == 0) {
+				count++;
+			} 
+			if (sums.containsKey(currSum)) {
+				count += sums.get(currSum);
+			}
+			if(sums.containsKey(currSum)) {
+				int occurrence = sums.get(currSum);
+				sums.put(currSum, occurrence + 1);
+			} else {sums.put(currSum , 1);}
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(subArraySumZero(new int[] { 1, 4, 20, 3, 10, 5 }));
-		System.out.println(subArraySumZero(new int[] { 10, 2, -2, -20, 10 }));
-		System.out.println(subArraySumZero(new int[] { 10, 0, 2, -2, -20, 10 }));
-		System.out.println(subArraySumZero(new int[] { 10, 0, 2, -2, 10, 10 }));
-		System.out.println(subArraySumZero(new int[] { 15, -2, 2, -8, 1, 7, 10, 23 }));
-		System.out.println(subArraySumZero(new int[] { 0, 0, 7, 0, 0 }));
+		// System.out.println(subArraySumZero(new int[] { 1, 4, 20, 3, 10, 5 }));
+		// System.out.println(subArraySumZero(new int[] { 10, 2, -2, -20, 10 }));
+		// System.out.println(subArraySumZero(new int[] { 10, 0, 2, -2, -20, 10 }));
+		// System.out.println(subArraySumZero(new int[] { 10, 0, 2, -2, 10, 10 }));
+		// System.out.println(subArraySumZero(new int[] { 15, -2, 2, -8, 1, 7, 10, 23
+		// }));
+		System.out.println(subArraySumZeroCount(new int[] { 0, 0, 7,-7, 0, 0 }));
+		System.out.println(subArraySumZeroCount(new int[] { 0, 0, 5, 5, 0, 0 }));
+		System.out.println(subArraySumZeroCount(new int[] { 6, -1, -3,4 ,-2 ,2,4,6,-12, -7  }));
 	}
 }
--- src/main/java/com/tasks/hashing/AllPairsWithGivenSum.java
+++ src/main/java/com/tasks/hashing/AllPairsWithGivenSum.java
@@ -7,11 +7,32 @@ package com.tasks.hashing;
  * @author Polina Koleva
  *
  */
+import java.util.*;
+
 public class AllPairsWithGivenSum {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void allPairsWithSum(int[] arr1, int[] arr2, int sum) {
+		boolean print = false;
+		HashSet<Integer> arr1Values = new HashSet<Integer>();
+		for (int i = 0; i < arr1.length; i++) {
+			arr1Values.add(arr1[i]);
+		}
+		for (int i = 0; i < arr2.length; i++) {
+			int difference = sum - arr2[i];
+			if(arr1Values.contains(difference)) {
+				// a pair found
+				if(!print) {
+					System.out.print(difference +" "+ arr2[i]);
+					print = true;
+				} else {
+					System.out.print(", "+difference +" "+ arr2[i]);
+				}
+			}
+		}
 	}
 
+	public static void main(String[] args) {
+		allPairsWithSum(new int[] {1,2,4,5,7}, new int[] {5,6,3,4,8}, 9);
+		allPairsWithSum(new int[] {0,2}, new int[] {1,3}, 3);
+	}
 }
